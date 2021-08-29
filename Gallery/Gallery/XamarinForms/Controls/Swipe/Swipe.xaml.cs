22ebe03674360441817b8872e40a0faa0c2a6904
Guilherme Anselmi <60523666+GuilhermeAnselmi@users.noreply.github.com>
2021-08-29 19:46:42 -0300

    Update

--- Gallery/Gallery/XamarinForms/Controls/Swipe/Swipe.xaml.cs
+++ Gallery/Gallery/XamarinForms/Controls/Swipe/Swipe.xaml.cs
@@ -16,5 +16,35 @@ namespace Gallery.XamarinForms.Controls.Swipe
         {
             InitializeComponent();
         }
+
+        private void SwipeItem_Invoked(object sender, EventArgs e)
+        {
+            lblDetalhe.Text = "Detalhes: " + DateTime.Now;
+        }
+
+        private void SwipeItem_Invoked_1(object sender, EventArgs e)
+        {
+            lblEditar.Text = "Editar: " + DateTime.Now;
+        }
+
+        private void SwipeItem_Invoked_2(object sender, EventArgs e)
+        {
+            lblExcluir.Text = "Excluir: " + DateTime.Now;
+        }
+
+        private void SwipeView_SwipeStarted(object sender, SwipeStartedEventArgs e)
+        {
+            lblStart.Text = "Start: " + DateTime.Now + " : " + e.SwipeDirection;
+        }
+
+        private void SwipeView_SwipeChanging(object sender, SwipeChangingEventArgs e)
+        {
+            lblChange.Text = "Change: " + DateTime.Now + " : " + e.Offset;
+        }
+
+        private void SwipeView_SwipeEnded(object sender, SwipeEndedEventArgs e)
+        {
+            lblEnd.Text = "End: " + DateTime.Now + " : " + e.SwipeDirection;
+        }
     }
 }
